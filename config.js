a93be2b00d5b54425e27257db1104b3ec531c38d
jakejooyoung <jakejooyoung@gmail.com>
2018-03-16 17:59:07 -0700

    Add config.js and add twitter api keys provided by twitter app generator.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'AkIuP7kay1Y44aWZgKVEMkq8e',
+  consumer_secret: '9YBrArQ3lkDPPolzPR4T3rZeKZuriZ60hcG6aRBe0INAtrvIaR',
+  access_token_key: '854479120054370304-dJ9pOlS0Y4EvrnP6eJO30HAtUpjUFWg',
+  access_token_secret: '1CIs0EgFn4dgMdH1zIkSGNEGLGSIcm4zIQmCxyH3DEril'
+}

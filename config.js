83a7dc95cac70d22d58d13e19cbad1ad6fddc6ec
jakejooyoung <jakejooyoung@gmail.com>
2018-03-17 03:18:43 -0700

    Add dotenv as dependency. Removing secrets from repo.

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+var dotenv = require('dotenv').config();
+
 module.exports = {
-  consumer_key: 'AkIuP7kay1Y44aWZgKVEMkq8e',
-  consumer_secret: '9YBrArQ3lkDPPolzPR4T3rZeKZuriZ60hcG6aRBe0INAtrvIaR',
-  access_token_key: '854479120054370304-dJ9pOlS0Y4EvrnP6eJO30HAtUpjUFWg',
-  access_token_secret: '1CIs0EgFn4dgMdH1zIkSGNEGLGSIcm4zIQmCxyH3DEril'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret:process.env.ACCESS_TOKEN_SECRET
 }

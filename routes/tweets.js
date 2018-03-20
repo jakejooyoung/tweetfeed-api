629fe12e05bd48b78b1cc14295de4886f6088a08
Jake Kim <jake@npgains.com>
2018-03-20 12:59:00 -0700

    Use async await to handle promises.

--- routes/tweets.js
+++ routes/tweets.js
@@ -8,49 +8,74 @@ const filter = {
   result_type: 'recent',
   lang: 'en'
 }
-const loadTweets= (filter) => {
-  return new Promise((resolve, reject) => {
-    let a=twitter.get('search/tweets', filter, function(err, data, response){
-      if(!err){
-        // console.log(data.statuses);
-        resolve(data.statuses);
-      } else {
-        reject(Error("It broke"));
-      }
-    });
-  });
-};
-const pruneTweets = (feed) => {
-    let newArray=[];
+
+const loadTweets = async (filter, res) => {
+  let data, feed;
+  try {
+    data = await twitter.get('search/tweets', filter);
+    feed = prune(data.statuses);
+  } catch (err) {
+    console.log('ERROR :', err)
+  }
+  return res.json(feed);
+}
+
+// const loadTweets= (filter) => {
+//   return new Promise((resolve, reject) => {
+//     let a=twitter.get('search/tweets', filter, function(err, data, response){
+//       if(!err){
+//         // console.log(data.statuses);
+//         resolve(data.statuses);
+//       } else {
+//         reject(Error("It broke"));
+//       }
+//   });
+//     });
+// };
+
+const prune = (feed) => {
+    let arr=[];
     for (let i = 0; i < feed.length; i++){
-      let tweet = { 
+      arr.push({
         id: feed[i].id_str ,
         userName:feed[i].user.name,
         userHandle:feed[i].user.screen_name,
         userImgUrl:feed[i].user.profile_image_url,
-        content:feed[i].text,
-      }
-      newArray.push(tweet);
+        content:feed[i].text
+      });
     }
-    return newArray;
+    return arr;
 };
 
 module.exports = app => {
+  app.get('/tweet', (req, res, next) => {
+    loadTweets(filter,res);
+  });
   app.get('/tweets', (req, res, next) => {
-    loadTweets(filter).then((feed) => {
-      let prunedFeed=pruneTweets(feed);
-      res.json(prunedFeed);
-    }).catch((error) => {
-      res.status(400);
-    });
+    loadTweets(filter,res);
   });
   app.get('/tweets/:hashtag', (req, res, next) => {
     filter.q=req.params.hashtag;
-    loadTweets(filter).then((feed) => {
-      let prunedFeed=pruneTweets(feed);
-      res.json(prunedFeed);
-    }).catch((error) => {
-      res.status(400);
-    });
+    loadTweets(filter,res);
   });
 };
+
+// module.exports = app => {
+//   app.get('/tweets', (req, res, next) => {
+//     loadTweets(filter).then((feed) => {
+//       let prunedFeed=pruneTweets(feed);
+//       res.json(prunedFeed);
+//     }).catch((error) => {
+//       res.status(400);
+//     });
+//   });
+//   app.get('/tweets/:hashtag', (req, res, next) => {
+//     filter.q=req.params.hashtag;
+//     loadTweets(filter).then((feed) => {
+//       let prunedFeed=pruneTweets(feed);
+//       res.json(prunedFeed);
+//     }).catch((error) => {
+//       res.status(400);
+//     });
+//   });
+// };
